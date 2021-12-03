0320f4221c0b5823b450b207022ee06e2f34c264
Aisa03 <alessioaisa03@gmail.com>
2021-12-03 14:04:30 +0100

    Situazione iniziale

---
+++ app/src/main/java/com/example/a5ainf_03_12_21/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a5ainf_03_12_21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
