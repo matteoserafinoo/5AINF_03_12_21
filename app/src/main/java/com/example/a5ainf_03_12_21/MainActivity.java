d801161e26b1402c8584f1c93c0d4e65cc020265
Aisa03 <alessioaisa03@gmail.com>
2021-12-03 14:41:57 +0100

    Creazione di list view e popolazione di essa.

--- app/src/main/java/com/example/a5ainf_03_12_21/MainActivity.java
+++ app/src/main/java/com/example/a5ainf_03_12_21/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.a5ainf_03_12_21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listas; //creazione di un oggetto lista view
+    String aStati[] = {"Italia", "Francia", "Germania", "Spagna"}; //creazione di un oggetto tipo array, inizializzato
+    //array adapter = oggetto con array, che permette di legare strettamante un array adapter alla list view, cosi al cambiare dell'array cambia anche la lista view
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listas = (ListView)findViewById(R.id.listastati); //il cast per trasformarlo in un oggetto list view
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,R.layout.activity_main,R.id.listastati,aStati);
+        listas.setAdapter(aa);
+
     }
 }
