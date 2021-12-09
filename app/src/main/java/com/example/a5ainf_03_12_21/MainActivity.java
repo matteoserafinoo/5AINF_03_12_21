033bd58d2c0e5164cf5c649a0397f8596531d083
matteoserafinoo <matteoserafino8@gmail.com>
2021-12-09 08:50:29 +0100

    Aggiornamento al 09/12/21, collegamento del bottone tramite id e regitrazione di un ascoltatore tramite click

--- app/src/main/java/com/example/a5ainf_03_12_21/MainActivity.java
+++ app/src/main/java/com/example/a5ainf_03_12_21/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.a5ainf_03_12_21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     //array adapter = oggetto con array, che permette di legare strettamante un array adapter alla list view, cosi al cambiare dell'array cambia anche la lista view
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
         listas = (ListView)findViewById(R.id.listastati); //il cast per trasformarlo in un oggetto list view
         ArrayAdapter<String> aa = new ArrayAdapter<String>(this,R.layout.activity_main,R.id.listastati,aStati);
+
         listas.setAdapter(aa);
+
+        //recuperato un riferimento del bottone (tramite l'id) e registrato un ascoltatore al click
+        final Button bb = (Button)findViewById(R.id.bottone);
+        bb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
